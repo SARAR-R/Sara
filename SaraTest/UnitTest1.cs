d9aea7245d6b8c764ee66f57200082a2789032c4
Saraswathi Rajkumar <srajkumar@clouddev.epiqcorp.com>
2018-08-28 20:19:00 -0700

    Add project files.

---
+++ SaraTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sara;
+
+namespace SaraTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Hello World!", "Hello World!");
+        }
+    }
+}
